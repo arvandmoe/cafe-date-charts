fbab81eec6a28d397831bb837d81b1bcaa9de8ca
Mohammad Khaki Bakhtiarvand <mdkhaki@yahoo.com>
2022-02-14 14:54:10 +0330

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <h1>Hello Vite!</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`
+
+// salam be doostane jadid !!!!!!!!!
+
+
---
+++ readme.md
@@ -0,0 +1,2 @@
+<!-- Share stuff Here -->
+[Basic ScatterPlot](https://www.d3-graph-gallery.com/graph/scatter_basic.html)
